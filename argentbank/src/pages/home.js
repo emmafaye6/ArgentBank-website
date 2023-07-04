be03663fd4204ea6c6e9f6666f6ba52fc7ac560b
Emma Faye <emmafaye06@gmail.com>
2023-07-04 15:15:21 +0200

    finished converting index and sign in page to react components

--- argentbank/src/pages/home.js
+++ argentbank/src/pages/home.js
@@ -1,4 +1,6 @@
 import Layout from "../components/home/layout";
+import Features from "../components/home/features/features";
+import Hero from "../components/home/hero/hero.js";
 import { useEffect } from "react";
 import "font-awesome/css/font-awesome.min.css";
 import "../styles/index.css";
@@ -7,6 +9,11 @@ function Home() {
   useEffect(() => {
     document.title = "Argent Bank - Home Page";
   }, []);
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Hero />
+      <Features />
+    </Layout>
+  );
 }
 export default Home;
--- argentbank/src/pages/signin.js
+++ argentbank/src/pages/signin.js
@@ -1,4 +1,37 @@
+import Layout from "../components/home/layout";
+import "./signin.css";
+
 function Signin() {
-  return <h1>hello this is signin</h1>;
+  return (
+    <Layout>
+      <div className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label for="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label for="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
+            <a href="./user.html" class="sign-in-button">
+              Sign In
+            </a>
+            {/* <!-- SHOULD BE THE BUTTON BELOW -->
+          <!-- <button class="sign-in-button">Sign In</button> -->
+          <!--  --> */}
+          </form>
+        </section>
+      </div>
+    </Layout>
+  );
 }
 export default Signin;
