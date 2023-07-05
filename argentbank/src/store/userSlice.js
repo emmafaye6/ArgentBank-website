82381318d27dceb472232ed0290922d658c629dc
Emma Faye <emmafaye06@gmail.com>
2023-07-05 13:13:58 +0200

    installed axios and used it to fetch log in

--- argentbank/src/store/userSlice.js
+++ argentbank/src/store/userSlice.js
@@ -1,21 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 export const loginUser = createAsyncThunk(
   "user/loginUser",
-  async (userCredentials, token) => {
-    const request = await fetch(
+  async (userCredentials) => {
+    const request = await axios.post(
       "http://localhost:3001/api/v1/user/login",
-      userCredentials,
-      {
-        method: "POST",
-        headers: {
-          accept: "application/json",
-          Authorization: "Bearer " + token,
-        },
-        body: JSON.stringify({
-          userCredentials,
-        }),
-      }
+      userCredentials
     );
     const response = await request.data.data;
     localStorage.setItem("user", JSON.stringify(response));
