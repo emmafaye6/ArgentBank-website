3d303ff9a5f547502eaade30a8f9a26ac7894c82
Emma Faye <emmafaye06@gmail.com>
2023-07-10 17:06:14 +0200

    made accounts elements into a map function

--- argentbank/src/components/user/accounts.js
+++ argentbank/src/components/user/accounts.js
@@ -1,40 +1,28 @@
 import "../user/accounts.css";
 import Button from "../layout/button";
+import { accountsData } from "../../data/accountsData";
 function Accounts() {
   return (
     <>
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className="transaction-button">View transactions</Button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className="transaction-button">View transactions</Button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className="transaction-button">View transactions</Button>
-        </div>
-      </section>
+      {accountsData.map((accounts) => {
+        return (
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">{accounts.title}</h3>
+              <p className="account-amount">{accounts.amount}</p>
+              <p className="account-amount-description">
+                {accounts.description}
+              </p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <Button className="transaction-button">View transactions</Button>
+            </div>
+          </section>
+        );
+      })}
     </>
   );
 }
+
 export default Accounts;
