46e5d735316e63da4c3e58bbe11acc3589c4dd49
Emma Faye <emmafaye06@gmail.com>
2023-07-11 11:24:41 +0200

    added ids to account components

--- argentbank/src/components/user/accounts.js
+++ argentbank/src/components/user/accounts.js
@@ -7,7 +7,7 @@ function Accounts() {
       <h2 className="sr-only">Accounts</h2>
       {accountsData.map((accounts) => {
         return (
-          <section className="account">
+          <section className="account" key={accounts.id}>
             <div className="account-content-wrapper">
               <h3 className="account-title">{accounts.title}</h3>
               <p className="account-amount">{accounts.amount}</p>
