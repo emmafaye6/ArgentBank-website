637d41a2b3ae2c93fcb6607bd868018f6bb1e1f8
Emma Faye <emmafaye06@gmail.com>
2023-07-20 15:44:16 +0200

    converted all images into webp

--- argentbank/src/components/layout/nav.js
+++ argentbank/src/components/layout/nav.js
@@ -1,5 +1,5 @@
 import "./nav.css";
-import Logo from "../../assets/img/argentBankLogo.png";
+import Logo from "../../assets/img/argentBankLogo.webp";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectLogIn } from "../../store/selectors";
--- argentbank/src/data/featuresData.js
+++ argentbank/src/data/featuresData.js
@@ -1,21 +1,21 @@
 export const featuresData = [
   {
     title: "You are our #1 priority",
-    image: require("../assets/img/icon-chat.png"),
+    image: require("../assets/img/icon-chat.webp"),
     imageAlt: "Chat icon",
     text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
     id: "1",
   },
   {
     title: "More savings means higher rates",
-    image: require("../assets/img/icon-money.png"),
+    image: require("../assets/img/icon-money.webp"),
     imageAlt: "Chat money",
     text: "The more you save with us, the higher your interest rate will be!",
     id: "2",
   },
   {
     title: "Security you can trust",
-    image: require("../assets/img/icon-security.png"),
+    image: require("../assets/img/icon-security.webp"),
     imageAlt: "Chat security",
     text: "We use top of the line encryption to make sure your data and money is always safe.",
     id: "3",
